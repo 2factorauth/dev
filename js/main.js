a9a99b5f3ae9f1448385585608b41fab127b3360
Carlgo11 <Carlgo11@users.noreply.github.com>
2020-04-19 06:19:34 +0200

    Add WEBP and rename desktop table

--- js/main.js
+++ js/main.js
@@ -1,31 +1,36 @@
 $(document).ready(function () {
-    lazyload();
+    var lazyLoadInstance = new LazyLoad({
+        elements_selector: ".lazyload"
+        // ... more custom settings?
+    });
 
-     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('service-worker.js');
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/service-worker.js');
     }
 
-    if (window.location.hash && window.location.hash.indexOf('#') > -1) {
+    const hash = window.location.hash;
+    if (hash && hash.indexOf('#') > -1) {
         $('.collapse').collapse('hide');
-        $('#' + window.location.hash.substring(1) + "-table").collapse("show");
-        $('#' + window.location.hash.substring(1) + "-mobile-table").collapse("show");
+        $('#' + hash.substring(1) + "-table").collapse("show");
+        $('#' + hash.substring(1) + "-mobile-table").collapse("show");
     }
 
     $('.exception').popup({
-            position: 'right center',
-            title: 'Exceptions & Restrictions'
-        });
+        position: 'right center',
+        title: 'Exceptions & Restrictions'
+    });
 });
 
 $('.cat').click(function () {
+    let hash = window.location.hash;
     // Collapse all other tables.
     $('.collapse').collapse('hide');
 
     // Check if category tables are displayed
-    if (!$('#' + window.location.hash.substring(1) + "-table").hasClass('collapsing') && !$('#' + window.location.hash.substring(1) + "-mobile-table").hasClass('collapsing')) {
+    if (!$('#' + hash.substring(1) + "-table").hasClass('collapsing') && !$('#' + hash.substring(1) + "-mobile-table").hasClass('collapsing')) {
         window.location.hash = this.id;
-        $('#' + window.location.hash.substring(1) + "-table").collapse("show");
-        $('#' + window.location.hash.substring(1) + "-mobile-table").collapse("show");
+        $('#' + hash.substring(1) + "-table").collapse("show");
+        $('#' + hash.substring(1) + "-mobile-table").collapse("show");
     } else {
         // Remove #category in URL
         history.pushState("", document.title, window.location.pathname + window.location.search);
