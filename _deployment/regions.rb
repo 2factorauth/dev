9d6d4e3f2704d4a995f38127a37db50b32f8e94b
Carlgo11 <Carlgo11@users.noreply.github.com>
2020-04-20 21:10:44 +0200

    Exclude about.html & README.md in config

--- _deployment/regions.rb
+++ _deployment/regions.rb
@@ -16,7 +16,6 @@ regions.each do |region|
     Dir.mkdir(dest_dir) unless File.exist?(dest_dir)
     files = Dir.glob('../*').reject { |file| file.end_with?('../.') }
     FileUtils.cp_r(files, dest_dir)
-    File.delete("#{dest_dir}/about.html") # Skip about.html as it requires GH access token.
   end
 
   # Category loop
