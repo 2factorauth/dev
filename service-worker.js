162b8376fba7359e6505589d06632a4fa3ea5180
Carlgo11 <Carlgo11@users.noreply.github.com>
2020-04-26 23:13:53 +0200

    Fix dynamic cache (thx @olback :pray:)

--- service-worker.js
+++ service-worker.js
@@ -1,94 +1,15 @@
-const CACHE_VERSION = 2;
-let CURRENT_CACHES = {
-    offline: 'offline-v' + CACHE_VERSION,
-    assets: 'assets-v' + CACHE_VERSION
-};
-const OFFLINE_URL = 'index.html';
-
-function createCacheBustedRequest(url) {
-    let request = new Request(url, {
-        cache: 'reload'
-    });
-    if ('cache' in request) {
-        return request;
-    }
-
-    // If {cache: 'reload'} didn't have any effect, append a cache-busting URL parameter instead.
-    let bustedUrl = new URL(url, self.location.href);
-    bustedUrl.search += (bustedUrl.search ? '&' : '') + 'cachebust=' + Date.now();
-    return new Request(bustedUrl);
-}
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        fetch(createCacheBustedRequest(OFFLINE_URL)).then(function (response) {
-            return caches.open(CURRENT_CACHES.offline).then(function (cache) {
-                return cache.put(OFFLINE_URL, response);
-            });
-        })
-    );
-    event.waitUntil(
-        caches.open(CURRENT_CACHES.assets).then(function (cache) {
-            return cache.addAll(
-                [
-                    "/",
-                    "/js/main.js",
-                    "/css/main.css"
-
-                ]
-            );
-        })
-    )
-});
-
-self.addEventListener('activate', event => {
-    let expectedCacheNames = Object.keys(CURRENT_CACHES).map(function (key) {
-        return CURRENT_CACHES[key];
-    });
-
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (expectedCacheNames.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
-
-self.addEventListener('fetch', event => {
-
+self.addEventListener('fetch', async event => {
     event.respondWith(
-        caches.open('dynamic-cache').then(function(cache) {
-            return cache.match(event.request).then(function (response) {
-                return response || fetch(event.request).then(function(response) {
-                    cache.put(event.request, response.clone());
-                    return response;
-                });
+        caches.open('dynamic-cache').then(async cache => {
+            let cacheResponse = await cache.match(event.request);
+            let fetchResponse = await fetch(event.request).then(async response => {
+                await cache.delete(event.request);
+                cache.put(event.request, response.clone());
+                return response;
+            }).catch(e => {
+                return cacheResponse;
             });
+            return fetchResponse;
         })
     );
-
-    if (event.request.mode === 'navigate' ||
-        (event.request.method === 'GET' &&
-            event.request.headers.get('accept').includes('text/html'))) {
-        event.respondWith(
-            fetch(event.request).catch(error => {
-                return caches.match(OFFLINE_URL);
-            })
-        );
-    }
-});
-
-self.addEventListener('fetch', function (event) {
-    if (!event.request.headers.get('accept').includes('text/html')) {
-        event.respondWith(
-            caches.match(event.request).then(function (response) {
-                return response || fetch(event.request);
-            })
-        );
-    }
 });
